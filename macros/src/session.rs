0feb15c1d595c20bc1f1a1626bfbfa14e2049550
Martin Vassor <martin.vassor@alumni.epfl.ch>
2021-09-30 14:51:46 +0100

    Add generic `Name`s and `Value`s in the types of `Send`, `Receive`, etc.
    Notice this modifies slightly the macro for the #[session] attribute for
    enums: one should now provide the types of names and values.
    
    E.g. #[session((), ())] for an enum that does not use the map.
    See `examples/3buyers.rs` for an example. I expect to extend this
    approach (to pass the types as macro parameters) in the future.

--- macros/src/session.rs
+++ macros/src/session.rs
@@ -4,6 +4,7 @@ use std::{collections::HashSet, mem};
 use syn::{
     parse::Nothing, parse2, parse_quote, punctuated::Punctuated, Error, Fields, GenericArgument,
     GenericParam, Ident, Index, Item, ItemEnum, ItemStruct, ItemType, PathArguments, Result, Type,
+    parse::ParseStream, parse::Parse, Token,
 };
 
 fn idents_set<P>(params: &Punctuated<GenericParam, P>) -> HashSet<Ident> {
@@ -139,7 +140,7 @@ fn session_struct(mut input: ItemStruct) -> Result<TokenStream> {
     Ok(quote!(#input #output))
 }
 
-fn session_enum(mut input: ItemEnum) -> Result<TokenStream> {
+fn session_enum(mut input: ItemEnum, name: Type, value: Type) -> Result<TokenStream> {
     if input.variants.is_empty() {
         let message = "expected at least one variant";
         return Err(Error::new_spanned(&input.variants, message));
@@ -226,9 +227,11 @@ fn session_enum(mut input: ItemEnum) -> Result<TokenStream> {
     output.extend(quote! {
         impl #impl_generics ::rumpsteak::Choices<'__r> for #ident #ty_generics #where_clause {
             type Role = __R;
+            type Name = #name;
+            type Value = #value;
 
             fn downcast(
-                state: ::rumpsteak::State<'__r, Self::Role>,
+                state: ::rumpsteak::State<'__r, Self::Role, Self::Name, Self::Value>,
                 message: <Self::Role as Role>::Message,
             ) -> ::core::result::Result<Self, <Self::Role as Role>::Message> {
                 #(let message = match ::rumpsteak::Message::downcast(message) {
@@ -249,12 +252,32 @@ fn session_enum(mut input: ItemEnum) -> Result<TokenStream> {
     Ok(quote!(#input #output))
 }
 
+struct SessionParams(Type, Type);
+
+impl Parse for SessionParams {
+    fn parse(content: ParseStream) -> Result<Self> {
+        let type1 = content.parse()?;
+        content.parse::<Token![,]>()?;
+        let type2 = content.parse()?;
+        Ok(SessionParams(type1, type2))
+    }
+}
+
 pub fn session(attr: TokenStream, input: TokenStream) -> Result<TokenStream> {
-    let Nothing = parse2(attr)?;
-    match parse2::<Item>(input)? {
-        Item::Type(input) => Ok(session_type(input)),
-        Item::Struct(input) => session_struct(input),
-        Item::Enum(input) => session_enum(input),
+    let input = parse2::<Item>(input)?;
+    match input {
+        Item::Type(input) => {
+            let Nothing = parse2(attr)?;
+            Ok(session_type(input))
+        },
+        Item::Struct(input) => {
+            let Nothing = parse2(attr)?;
+            session_struct(input)
+        },
+        Item::Enum(input) => {
+            let SessionParams(name, value) = parse2(attr)?;
+            session_enum(input, name, value)
+        },
         item => Err(Error::new_spanned(item, "expected a type, struct or enum")),
     }
 }
