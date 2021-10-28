a1f248b9e48d806c7c66ab361376997a2d1acd6c
Martin Vassor <martin.vassor@alumni.epfl.ch>
2021-10-28 10:54:34 +0100

    Add Incr effect

--- src/effect.rs
+++ src/effect.rs
@@ -28,3 +28,24 @@ impl<N, V> SideEffect for Constant<N, V> {
     type Name = N;
     type Value = V;
 }
+
+pub struct Incr<const NAME: char, const VALUE: i32> {
+    _p: PhantomData<()>,
+}
+
+impl<const NAME: char, const VALUE: i32> Default for Incr<NAME, VALUE> {
+    fn default() -> Self {
+        Incr {
+            _p: PhantomData,
+        }
+    }
+}
+
+impl<const NAME: char, const VALUE: i32> SideEffect for Incr<NAME, VALUE> {
+    type Name = char;
+    type Value = i32;
+
+    fn side_effect(&mut self, m: &mut HashMap<Self::Name, Self::Value>) {
+        m.get_mut(&NAME).map(|ref_v| (*ref_v) += VALUE);
+    }
+}
