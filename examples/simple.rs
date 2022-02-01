395bef4f27469079f26cfe2f749ac218086e293e
Martin Vassor <martin.vassor@alumni.epfl.ch>
2022-02-01 12:37:57 +0000

    Add an example created with AEPL

---
+++ examples/simple.rs
@@ -0,0 +1,1 @@
+simple_aepl/simple.rs
---
+++ examples/simple_aepl/simple.rs
@@ -0,0 +1,58 @@
+use aepl_rumpsteak_derive::from_epl;
+
+use futures::{
+    channel::mpsc::{UnboundedReceiver, UnboundedSender},
+    executor, try_join,
+};
+#[allow(unused_imports)]
+use ::rumpsteak::{
+    channel::Bidirectional, session, Branch, End, Message, Receive, Role, Roles, Select, Send, formula,
+};
+
+type Channel = Bidirectional<UnboundedSender<Label>, UnboundedReceiver<Label>>;
+type Name = char;
+type Value = u32;
+
+#[derive(Roles)]
+#[allow(dead_code)]
+struct Roles {
+    a: A,
+    b: B,
+}
+
+#[derive(Role)]
+#[message(Label)]
+struct A {
+    #[route(B)]
+    b: Channel,
+}
+
+#[derive(Role)]
+#[message(Label)]
+struct B {
+    #[route(A)]
+    a: Channel,
+}
+
+#[derive(Message)]
+enum Label {
+    Ping(Ping),
+}
+
+struct Ping(u32);
+
+#[session]
+type SimpleA = Send<B, Ping, End>;
+
+#[session]
+type SimpleB = Receive<A, Ping, End>;
+
+from_epl!("examples/simple_aepl/A.aepl", SimpleA, A);
+from_epl!("examples/simple_aepl/B.aepl", SimpleB, B);
+
+fn main() {
+    let Roles{ mut a, mut b } = Roles::default();
+    executor::block_on(async {
+        try_join!(A(&mut a), B(&mut b)).unwrap();
+    });
+}
