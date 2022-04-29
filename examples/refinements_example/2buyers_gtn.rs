c9a90f8be6d7d99a9109f83a5f91a0ff107c155b
Qi Xi <xiqi5703@gmail.com>
2022-04-29 11:48:03 +0100

    Add 2buyers refinement test case for GreaterThan constraint

---
+++ examples/refinements_example/2buyers_gtn.rs
@@ -0,0 +1,147 @@
+use futures::{
+    channel::mpsc::{UnboundedReceiver, UnboundedSender},
+    executor, try_join,
+};
+#[allow(unused_imports)]
+use ::rumpsteak::{
+    channel::Bidirectional,
+    session,
+    Branch,
+    End,
+    Message,
+    Receive,
+    Role,
+    Roles,
+    Select,
+    Send,
+    effect::{
+        SideEffect,
+        Constant,
+        Incr,
+    },
+    try_session,
+    predicate::{
+        Tautology,
+        LTnVar,
+        GTnVar
+    },
+};
+
+use std::collections::HashMap;
+use std::error::Error;
+
+type Channel = Bidirectional<UnboundedSender<Label>, UnboundedReceiver<Label>>;
+type Name = char;
+type Value = u32;
+
+
+// global protocol TwoBuyer(role A, role C)
+// {
+//     empty1(i32) from A to C; @ x > 10                                    
+//     choice at C
+//     {
+//         valid(i32) from C to A; @ y < 20
+//     }
+//     or
+//     {
+//         quit() from C to A;
+//     }
+// }
+
+#[derive(Roles)]
+#[allow(dead_code)]
+struct Roles {
+    c: C,
+    a: A,
+}
+
+#[derive(Role)]
+#[message(Label)]
+struct C {
+    #[route(A)]
+    a: Channel,
+}
+
+#[derive(Role)]
+#[message(Label)]
+struct A {
+    #[route(C)]
+    c: Channel,
+}
+
+#[derive(Message)]
+enum Label {
+    Empty1(Empty1),
+    Valid(Valid),
+    Quit(Quit),
+}
+
+struct Empty1(u32);
+
+struct Valid(u32);
+
+struct Quit;
+
+#[session(Name, Value)]
+type TwoBuyersC = Receive<A, Empty1, Tautology<Name, Value>, Constant<Name, Value>, Select<A, Tautology<Name, Value>, Constant<Name, Value>, TwoBuyersC1>>;
+
+#[session(Name, Value)]
+enum TwoBuyersC1 {
+    Quit(Quit, End),
+    Valid(Valid, End),
+}
+
+#[session(Name, Value)]
+type TwoBuyersA = Send<C, Empty1, GTnVar<Value, 'x', 'y'>, Constant<Name, Value>, Branch<C, Tautology<Name, Value>, Constant<Name, Value>, TwoBuyersA1>>;
+
+#[session(Name, Value)]
+enum TwoBuyersA1 {
+    Quit(Quit, End),
+    Valid(Valid, End),
+}
+
+async fn TwoBuyersA(role: &mut A) -> Result<(), Box<dyn Error>> {
+    let mut map = HashMap::new();
+    map.insert('x', 20);
+    map.insert('y', 10);
+
+    try_session(role, map,
+    |mut s: TwoBuyersA<'_, _>| async {
+        let s = s.send(Empty1(5)).await?;
+        
+        match s.branch().await? {
+            TwoBuyersA1::Quit(_, s) => {
+                Ok(((), s))
+            }
+            TwoBuyersA1::Valid(_, s) => {
+                Ok(((), s))
+            }
+        }
+    })
+    .await
+}
+
+async fn TwoBuyersC(role: &mut C) -> Result<(), Box<dyn Error>> {
+    let mut map = HashMap::new();
+    map.insert('x', 0);
+    map.insert('y', 10);
+
+    try_session(role, map,
+    |mut s: TwoBuyersC<'_, _>| async {
+        let (Empty1(m), s) = s.receive().await?;
+        let s = s.select(Valid(7)).await?;
+        Ok(((), s))
+    })
+    .await
+}
+
+fn main() {
+    let mut roles = Roles::default();
+    executor::block_on(async {
+        try_join!(
+            TwoBuyersA(&mut roles.a),
+            TwoBuyersC(&mut roles.c),
+        )
+        .unwrap();
+    });
+}
